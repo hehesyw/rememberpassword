e761ffbfdb0933b8dd7317d6f5ed71aff3f29c76
wenscript <534727769@qq.com>
2015-07-01 16:39:39 +0800

    learning finish

--- src/com/syw/rememberpassword/MainActivity.java
+++ src/com/syw/rememberpassword/MainActivity.java
@@ -1,35 +1,61 @@
 package com.syw.rememberpassword;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
-
-public class MainActivity extends Activity {
-
+/*
+ * 本程序需要实现一个记住密码的功能,主要是利用SharedPreferences对象将程序中的密码存储起来
+ * 启动的时候通过判断是勾选复选框，来判断是否加载密码
+ */
+public class MainActivity extends Activity implements OnClickListener{
+	private EditText account_edit;
+	private EditText password_edit;
+	private CheckBox checkBox;
+	private Button login_button;
+	private SharedPreferences pref;
+	private SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        pref=getSharedPreferences("saveInfo",MODE_PRIVATE);
+        account_edit=(EditText)findViewById(R.id.accountEdit);
+        password_edit=(EditText)findViewById(R.id.passwordEdit);
+        checkBox=(CheckBox)findViewById(R.id.checkbox);
+        login_button=(Button)findViewById(R.id.buttonId);
+        
+        Boolean b=pref.getBoolean("isRemember",false);
+        Log.d("TAG","bb");
+        if(b){
+        	String account=pref.getString("account","");
+        	String password=pref.getString("password","");
+        	Log.d("TAG","bb");
+        	account_edit.setText(account);
+        	password_edit.setText(password);
+        	checkBox.setChecked(true);
         }
-        return super.onOptionsItemSelected(item);
+        login_button.setOnClickListener(this);
     }
+	@Override
+	public void onClick(View v) {
+		String account=account_edit.getText().toString();
+		String password=password_edit.getText().toString();
+		if(account.equals("admin")&&password.equals("123456")){
+			if(checkBox.isChecked()){//判断复选框是否被选
+				editor=pref.edit();
+				editor.putString("account",account);
+				editor.putString("password",password);
+				editor.putBoolean("isRemember",true);
+				editor.commit();
+			}
+		}
+	}
+
 }
